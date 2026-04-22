46307e5bc1889ab57590dcb24aafe609c9da85cf
luanaruffinelli <luanaruffinelli02@proton.me>
2026-04-21 23:08:01 -0300

    Add routing configuration and update index page layout

---
+++ src/core/utils/router.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+    DASHBOARD: {
+        path: "/",
+        name: "Panel",
+        public: false,
+    },
+    LOGIN: {
+        path: "/auth/login",
+        name: "Iniciar sesion",
+        public: true,
+    }
+}
