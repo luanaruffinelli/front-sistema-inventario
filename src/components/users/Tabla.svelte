186ba0ed7884542ad063c3a4a0d01f3eb71e3af6
luanaruffinelli <luanaruffinelli02@proton.me>
2026-05-05 22:55:10 -0300

    Add logo image to public directory

--- src/components/users/Tabla.svelte
+++ src/components/users/Tabla.svelte
@@ -12,19 +12,30 @@
     });
 </script>
 
-<table>
+<table class="flex-1 width-100">
     <thead>
         <tr>
-            <th>Nombre completo</th>
-            <th>Email</th>
+            <th class="border border-red-500 text-left">Nombre completo</th>
+            <th class="border border-red-500">Email</th>
+            <th class="border border-red-500">Acciones</th>
         </tr>
     </thead>
 
     <tbody>
         {#each users as user}
             <tr>
-                <td>{user.firstName} {user.lastName}</td>
-                <td>{user.email}</td>
+                <td class="border border-blue-500">{user.firstName} {user.lastName}</td>
+                <td class="border border-blue-500">{user.email}</td>
+                <td class="border border-blue-500">
+                    <div class="flex justify-center gap-2">
+                        <button aria-label="Editar" class="bg-gray-500 hover:bg-gray-600 dark:bg-gray-300 dark:hover:bg-gray-500  text-white font-bold py-2 px-4 rounded dark:bg-gray-700 dark:hover:bg-gray-600">
+                            Editar
+                        </button>
+                        <button aria-label="Eliminar" class="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded dark:bg-gray-800 dark:hover:bg-gray-600">
+                            Eliminar
+                        </button>
+                    </div>
+                </td>
             </tr>
         {/each}
     </tbody>
