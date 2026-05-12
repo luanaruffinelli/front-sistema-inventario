3ad2de07289d2f95a3b8a7c8a191d0f5c197993f
luanaruffinelli <luanaruffinelli02@proton.me>
2026-05-11 22:32:45 -0300

    Función para eliminar usuarios

---
+++ src/components/users/Delete.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+
+    let { user, deleteDialog = $bindable(false) } = $props();
+
+</script>
+
+{#if deleteDialog}
+    <dialog open={deleteDialog} class="w-full h-full top-0 left-0 fixed flex items-center bg-transparent justify-center backdrop-blur-xl">
+        <div class="bg-white p-4 rounded-md w-100 dark:bg-gray-800 relative">
+            <button 
+                aria-label='Cerrar' 
+                class="close" 
+                onclick={() => deleteDialog = false}>x</button
+            >
+            <div class="p-4">
+                <h2 class="text-lg font-bold">Desea eliminar el usuario {user.firstName} {user.lastName}?</h2>
+            </div>
+        </div>
+        
+    </dialog>
+
+    <dialog open={deleteDialog} class="w-full h-full top-0 left-0 fixed flex items-center bg-transparent justify-center backdrop-blur-xl">
+        <div class="bg-white p-4 rounded-md w-100 dark:bg-gray-800 relative">
+            <button 
+                aria-label='Cerrar' 
+                class="close" 
+                onclick={() => deleteDialog = false}>x</button
+            >
+            <div class="p-4">
+                <h2 class="text-lg font-bold">Desea eliminar el usuario {user.firstName} {user.lastName}?</h2>
+            </div>
+        </div>
+        
+    </dialog>
+{/if}
--- src/components/users/Tabla.svelte
+++ src/components/users/Tabla.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+    import Delete from "./Delete.svelte";
     import { onMount } from "svelte";
 
+    let userSelected = $state([]);
+    let deleteDialog = $state(false);
     let users: any = $state([]);
 
     onMount(async () => {
@@ -12,28 +15,34 @@
     });
 </script>
 
+<Delete bind:deleteDialog user={userSelected} />
+
 <table class="flex-1 width-100">
     <thead>
         <tr>
-            <th class="border border-red-500 text-left">Nombre completo</th>
-            <th class="border border-red-500">Email</th>
-            <th class="border border-red-500">Acciones</th>
+            <th class="">Nombre completo</th>
+            <th class="">Email</th>
+            <th class="">Acciones</th>
         </tr>
     </thead>
 
     <tbody>
         {#each users as user}
             <tr>
-                <td class="border border-blue-500">{user.firstName} {user.lastName}</td>
-                <td class="border border-blue-500">{user.email}</td>
-                <td class="border border-blue-500">
+                <td class="">{user.firstName} {user.lastName}</td>
+                <td class="">{user.email}</td>
+                <td class="">
                     <div class="flex justify-center gap-2">
                         <button aria-label="Editar" class="bg-gray-500 hover:bg-gray-600 dark:bg-gray-300 dark:hover:bg-gray-500  text-white font-bold py-2 px-4 rounded dark:bg-gray-700 dark:hover:bg-gray-600">
                             Editar
                         </button>
-                        <button aria-label="Eliminar" class="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded dark:bg-gray-800 dark:hover:bg-gray-600">
-                            Eliminar
-                        </button>
+                        <button 
+                            aria-label="Eliminar" 
+                            onclick={() => (deleteDialog = true, userSelected = user)}
+                                class="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded 
+                                dark:bg-gray-800 dark:hover:bg-gray-600">
+                            Eliminar</button
+                        >
                     </div>
                 </td>
             </tr>
